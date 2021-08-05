6393f7bcb68d68005249287349a0be33d45fda02
Marvin Bernd <marvinbernd@MacBook-Pro-von-Marvin.local>
2021-08-05 14:25:09 +0200

    first commit

---
+++ subscriber/index.js
@@ -0,0 +1,28 @@
+const kafka = require("kafka-node");
+
+const client = new kafka.KafkaClient({
+  kafkaHost:
+    process.env.ENVIRONMENT === "local"
+      ? process.env.INTERNAL_KAFKA_ADDR
+      : process.env.EXTERNAL_KAFKA_ADDR,
+});
+const Consumer = kafka.Consumer;
+
+const consumer = new Consumer(
+  client,
+  [
+    {
+      topic: process.env.TOPIC,
+      partition: o,
+    },
+  ],
+  { autoCommit: false }
+);
+
+consumer.on("message", (message) => {
+  console.log(message);
+});
+
+consumer.on("error", (err) => {
+  console.log(err);
+});
